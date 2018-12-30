11a5e897c7d2052cd4f7e1c7cf34ce5a92079430
Tim Savage <tim.savage@gmail.com>
2018-12-30 07:49:37 -0600

    reformat code

--- HomeStar/src/main/java/com/winterhaven_mc/homestar/messages/MessageManager.java
+++ HomeStar/src/main/java/com/winterhaven_mc/homestar/messages/MessageManager.java
@@ -14,10 +14,9 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Implements message manager for HomeStar
- * 
- * @author      Tim Savage
- * @version		1.0
- *  
+ *
+ * @author Tim Savage
+ * @version 1.0
  */
 public final class MessageManager extends AbstractMessageManager {
 
@@ -39,101 +38,101 @@ public final class MessageManager extends AbstractMessageManager {
 
 
 	@Override
-	protected Map<String,String> getDefaultReplacements(CommandSender recipient) {
+	protected Map<String, String> getDefaultReplacements(CommandSender recipient) {
 
-		Map<String,String> replacements = new HashMap<>();
+		Map<String, String> replacements = new HashMap<>();
 
 		// strip color codes
-		replacements.put("%PLAYER_NAME%",ChatColor.stripColor(recipient.getName()));
-		replacements.put("%WORLD_NAME%",ChatColor.stripColor(getWorldName(recipient)));
+		replacements.put("%PLAYER_NAME%", ChatColor.stripColor(recipient.getName()));
+		replacements.put("%WORLD_NAME%", ChatColor.stripColor(getWorldName(recipient)));
 		replacements.put("%ITEM_NAME%", ChatColor.stripColor(getItemName()));
-		replacements.put("%QUANTITY%","1");
-		replacements.put("%MATERIAL%","unknown");
-		replacements.put("%DESTINATION_NAME%",ChatColor.stripColor(getSpawnDisplayName()));
-		replacements.put("%TARGET_PLAYER%","target player");
+		replacements.put("%QUANTITY%", "1");
+		replacements.put("%MATERIAL%", "unknown");
+		replacements.put("%DESTINATION_NAME%", ChatColor.stripColor(getSpawnDisplayName()));
+		replacements.put("%TARGET_PLAYER%", "target player");
 		replacements.put("%WARMUP_TIME%",
 				getTimeString(TimeUnit.SECONDS.toMillis(plugin.getConfig().getInt("teleport-warmup"))));
 
 		// leave color codes intact
-		replacements.put("%player_name%",recipient.getName());
-		replacements.put("%world_name%",getWorldName(recipient));
-		replacements.put("%item_name%",getItemName());
-		replacements.put("%destination_name%",getSpawnDisplayName());
-		replacements.put("%target_player%","target player");
+		replacements.put("%player_name%", recipient.getName());
+		replacements.put("%world_name%", getWorldName(recipient));
+		replacements.put("%item_name%", getItemName());
+		replacements.put("%destination_name%", getSpawnDisplayName());
+		replacements.put("%target_player%", "target player");
 
 		// if recipient is player, get remaining cooldown time from teleport manager
 		if (recipient instanceof Player) {
 			replacements.put("%COOLDOWN_TIME%",
-					getTimeString(plugin.teleportManager.getCooldownTimeRemaining((Player)recipient)));
+					getTimeString(plugin.teleportManager.getCooldownTimeRemaining((Player) recipient)));
 		}
 		else {
-			replacements.put("%COOLDOWN_TIME%",getTimeString(0L));
+			replacements.put("%COOLDOWN_TIME%", getTimeString(0L));
 		}
 
 		return replacements;
 	}
 
 	/**
-	 *  Send message to player
-	 * 
-	 * @param recipient		player receiving message
-	 * @param messageId		message identifier in messages file
+	 * Send message to player
+	 *
+	 * @param recipient player receiving message
+	 * @param messageId message identifier in messages file
 	 */
 	public final void sendMessage(final CommandSender recipient, final MessageId messageId) {
 
-		Map<String,String> replacements = getDefaultReplacements(recipient);
+		Map<String, String> replacements = getDefaultReplacements(recipient);
 
 		//noinspection unchecked
-		sendMessage(recipient,messageId,replacements);
+		sendMessage(recipient, messageId, replacements);
 	}
 
-	
+
 	/**
-	 *  Send message to player
-	 * 
-	 * @param recipient			player receiving message
-	 * @param messageId			message identifier in messages file
+	 * Send message to player
+	 *
+	 * @param recipient player receiving message
+	 * @param messageId message identifier in messages file
 	 */
 	public final void sendMessage(final CommandSender recipient,
 								  final MessageId messageId,
 								  final String destinationName) {
 
-		Map<String,String> replacements = getDefaultReplacements(recipient);
+		Map<String, String> replacements = getDefaultReplacements(recipient);
 
-		replacements.put("%destination_name%",destinationName);
-		replacements.put("%DESTINATION_NAME%",ChatColor.stripColor(destinationName));
+		replacements.put("%destination_name%", destinationName);
+		replacements.put("%DESTINATION_NAME%", ChatColor.stripColor(destinationName));
 
 		//noinspection unchecked
-		sendMessage(recipient,messageId,replacements);
+		sendMessage(recipient, messageId, replacements);
 	}
 
-	
+
 	/**
 	 * Send message to player
-	 * 
-	 * @param recipient			player receiving message
-	 * @param messageId			message identifier in messages file
-	 * @param quantity			number of items
+	 *
+	 * @param recipient player receiving message
+	 * @param messageId message identifier in messages file
+	 * @param quantity  number of items
 	 */
 	public final void sendMessage(final CommandSender recipient,
 								  final MessageId messageId,
 								  final Integer quantity) {
 
-		Map<String,String> replacements = getDefaultReplacements(recipient);
+		Map<String, String> replacements = getDefaultReplacements(recipient);
 
-		replacements.put("%quantity%",quantity.toString());
-		replacements.put("%QUANTITY%",quantity.toString());
+		replacements.put("%quantity%", quantity.toString());
+		replacements.put("%QUANTITY%", quantity.toString());
 
 		//noinspection unchecked
-		sendMessage(recipient,messageId,replacements);
+		sendMessage(recipient, messageId, replacements);
 	}
 
-	
+
 	/**
 	 * Send message to player
-	 * 
-	 * @param recipient			player recieving message
-	 * @param messageId			message identifier in messages file
+	 *
+	 * @param recipient player recieving message
+	 * @param messageId message identifier in messages file
 	 */
 	@SuppressWarnings("unused")
 	final void sendMessage(final CommandSender recipient,
@@ -141,15 +140,15 @@ public final class MessageManager extends AbstractMessageManager {
 						   final Integer quantity,
 						   final Player targetPlayer) {
 
-		Map<String,String> replacements = getDefaultReplacements(recipient);
+		Map<String, String> replacements = getDefaultReplacements(recipient);
 
-		replacements.put("%quantity%",quantity.toString());
-		replacements.put("%QUANTITY%",quantity.toString());
-		replacements.put("%target_player%",targetPlayer.getName());
-		replacements.put("%TARGET_PLAYER%",ChatColor.stripColor(targetPlayer.getName()));
+		replacements.put("%quantity%", quantity.toString());
+		replacements.put("%QUANTITY%", quantity.toString());
+		replacements.put("%target_player%", targetPlayer.getName());
+		replacements.put("%TARGET_PLAYER%", ChatColor.stripColor(targetPlayer.getName()));
 
 		//noinspection unchecked
-		sendMessage(recipient,messageId,replacements);
+		sendMessage(recipient, messageId, replacements);
 	}
 
 }
