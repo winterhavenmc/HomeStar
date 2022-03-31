bfd78729abeadd527f7a3ebf4e38d4f242c3cfde
Tim Savage <tim.savage@gmail.com>
2022-03-31 02:06:15 -0500

    extract methods for each line of status message

--- src/main/java/com/winterhavenmc/homestar/commands/StatusSubcommand.java
+++ src/main/java/com/winterhavenmc/homestar/commands/StatusSubcommand.java
@@ -66,54 +66,110 @@ final class StatusSubcommand extends AbstractSubcommand {
 		}
 
 		// output config settings
-		String versionString = this.plugin.getDescription().getVersion();
-		sender.sendMessage(ChatColor.DARK_AQUA + "[HomeStar] "
-				+ ChatColor.AQUA + "Version: " + ChatColor.RESET + versionString);
+		showPluginVersion(sender);
+		showDebugSetting(sender);
+		showLanguageSetting(sender);
+		showDefaultMaterialSetting(sender);
+		showMinimumDistanceSetting(sender);
+		showTeleportWarmupSetting(sender);
+		showTeleportCooldownSetting(sender);
+		showLeftClickAllowedSetting(sender);
+		showShiftClickRequiredSetting(sender);
+		showCancelOnMovementSetting(sender);
+		showRemoveFromInventorySetting(sender);
+		showAllowInRecipesSetting(sender);
+		showLightningSetting(sender);
+		showEnabledWorlds(sender);
 
+		return true;
+	}
+
+
+	private void showPluginVersion(final CommandSender sender) {
+		sender.sendMessage(ChatColor.DARK_AQUA + "[HomeStar] "
+				+ ChatColor.AQUA + "Version: " + ChatColor.RESET + this.plugin.getDescription().getVersion());
+	}
+
+
+	private void showDebugSetting(final CommandSender sender) {
 		if (plugin.getConfig().getBoolean("debug")) {
 			sender.sendMessage(ChatColor.DARK_RED + "DEBUG: true");
 		}
+	}
 
+
+	private void showLanguageSetting(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN + "Language: "
 				+ ChatColor.RESET + plugin.getConfig().getString("language"));
+	}
 
+
+	private void showDefaultMaterialSetting(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN + "Default material: "
 				+ ChatColor.RESET + plugin.getConfig().getString("item-material"));
+	}
 
+
+	private void showMinimumDistanceSetting(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN + "Minimum distance: "
 				+ ChatColor.RESET + plugin.getConfig().getInt("minimum-distance"));
+	}
 
+
+	private void showTeleportWarmupSetting(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN + "Warmup: " + ChatColor.RESET
 				+ plugin.messageBuilder.getTimeString(SECONDS.toMillis(plugin.getConfig().getInt("teleport-warmup"))));
+	}
 
+
+	private void showTeleportCooldownSetting(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN + "Cooldown: " + ChatColor.RESET
 				+ plugin.messageBuilder.getTimeString(SECONDS.toMillis(plugin.getConfig().getInt("teleport-cooldown"))));
+	}
 
+
+	private void showLeftClickAllowedSetting(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN + "Left-click allowed: "
 				+ ChatColor.RESET + plugin.getConfig().getBoolean("left-click"));
+	}
 
+
+	private void showShiftClickRequiredSetting(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN + "Shift-click required: "
 				+ ChatColor.RESET + plugin.getConfig().getBoolean("shift-click"));
+	}
 
+
+	private void showCancelOnMovementSetting(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN
 				+ "Cancel on damage/movement/interaction: " + ChatColor.RESET + "[ "
 				+ plugin.getConfig().getBoolean("cancel-on-damage") + "/"
 				+ plugin.getConfig().getBoolean("cancel-on-movement") + "/"
 				+ plugin.getConfig().getBoolean("cancel-on-interaction") + " ]");
+	}
 
+
+	private void showRemoveFromInventorySetting(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN + "Remove from inventory: "
 				+ ChatColor.RESET + plugin.getConfig().getString("remove-from-inventory"));
+	}
 
+
+	private void showAllowInRecipesSetting(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN + "Allow in recipes: "
 				+ ChatColor.RESET + plugin.getConfig().getBoolean("allow-in-recipes"));
+	}
 
+
+	private void showLightningSetting(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN + "Lightning: "
 				+ ChatColor.RESET + plugin.getConfig().getBoolean("lightning"));
+	}
 
+
+	private void showEnabledWorlds(final CommandSender sender) {
 		sender.sendMessage(ChatColor.GREEN + "Enabled Words: "
 				+ ChatColor.RESET + plugin.worldManager.getEnabledWorldNames().toString());
-
-		return true;
 	}
 
 }
