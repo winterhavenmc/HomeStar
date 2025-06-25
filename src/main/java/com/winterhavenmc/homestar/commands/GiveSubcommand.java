bb9468e3be839a65ac703f51d9729cf92c015419
Tim Savage <tim.savage@gmail.com>
2025-06-25 00:27:05 -0500

    edit for style

--- src/main/java/com/winterhavenmc/homestar/commands/GiveSubcommand.java
+++ src/main/java/com/winterhavenmc/homestar/commands/GiveSubcommand.java
@@ -18,30 +18,34 @@
 package com.winterhavenmc.homestar.commands;
 
 import com.winterhavenmc.homestar.PluginMain;
+import com.winterhavenmc.homestar.messages.Macro;
 import com.winterhavenmc.homestar.messages.MessageId;
 import com.winterhavenmc.homestar.sounds.SoundId;
-import com.winterhavenmc.homestar.messages.Macro;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 
-final class GiveSubcommand extends AbstractSubcommand {
-
+final class GiveSubcommand extends AbstractSubcommand
+{
 	private final PluginMain plugin;
 
 
 	/**
 	 * Class constructor
+	 *
 	 * @param plugin reference to plugin main class instance
 	 */
-	GiveSubcommand(final PluginMain plugin) {
+	GiveSubcommand(final PluginMain plugin)
+	{
 		this.plugin = Objects.requireNonNull(plugin);
 		this.name = "give";
 		this.permissionNode = "homestar.give";
@@ -54,12 +58,14 @@ final class GiveSubcommand extends AbstractSubcommand {
 
 	@Override
 	public List<String> onTabComplete(final CommandSender sender, final Command command,
-									  final String alias, final String[] args) {
+	                                  final String alias, final String[] args)
+	{
 
 		List<String> returnList = new ArrayList<>();
 
 		// return list of matching players
-		if (args.length == 2) {
+		if (args.length == 2)
+		{
 			return plugin.getServer().getOnlinePlayers().stream()
 					.map(HumanEntity::getName)
 					.filter(playerName -> matchPrefix(playerName, args[1]))
@@ -67,7 +73,8 @@ final class GiveSubcommand extends AbstractSubcommand {
 		}
 
 		// return some useful quantities
-		else if (args.length == 3) {
+		else if (args.length == 3)
+		{
 			returnList.add("1");
 			returnList.add("2");
 			returnList.add("3");
@@ -80,17 +87,19 @@ final class GiveSubcommand extends AbstractSubcommand {
 
 
 	@Override
-	public boolean onCommand(final CommandSender sender, final List<String> args) {
-
+	public boolean onCommand(final CommandSender sender, final List<String> args)
+	{
 		// if command sender does not have permission to give HomeStars, output error message and return true
-		if (!sender.hasPermission(permissionNode)) {
+		if (!sender.hasPermission(permissionNode))
+		{
 			plugin.messageBuilder.compose(sender, MessageId.PERMISSION_DENIED_GIVE).send();
 			plugin.soundConfig.playSound(sender, SoundId.COMMAND_FAIL);
 			return true;
 		}
 
 		// check min arguments
-		if (args.size() < getMinArgs()) {
+		if (args.size() < getMinArgs())
+		{
 			plugin.messageBuilder.compose(sender, MessageId.COMMAND_FAIL_ARGS_COUNT_UNDER).send();
 			plugin.soundConfig.playSound(sender, SoundId.COMMAND_FAIL);
 			displayUsage(sender);
@@ -98,7 +107,8 @@ final class GiveSubcommand extends AbstractSubcommand {
 		}
 
 		// check max arguments
-		if (args.size() > getMaxArgs()) {
+		if (args.size() > getMaxArgs())
+		{
 			plugin.messageBuilder.compose(sender, MessageId.COMMAND_FAIL_ARGS_COUNT_OVER).send();
 			plugin.soundConfig.playSound(sender, SoundId.COMMAND_FAIL);
 			displayUsage(sender);
@@ -112,7 +122,8 @@ final class GiveSubcommand extends AbstractSubcommand {
 		Player targetPlayer = plugin.getServer().getPlayer(targetPlayerName);
 
 		// if no match, send player not found message and return
-		if (targetPlayer == null) {
+		if (targetPlayer == null)
+		{
 			plugin.messageBuilder.compose(sender, MessageId.COMMAND_FAIL_PLAYER_NOT_FOUND).send();
 			plugin.soundConfig.playSound(sender, SoundId.COMMAND_FAIL);
 			return true;
@@ -120,11 +131,13 @@ final class GiveSubcommand extends AbstractSubcommand {
 
 		int quantity = 1;
 
-		if (args.size() > 1) {
-			try {
+		if (args.size() > 1)
+		{
+			try
+			{
 				quantity = Integer.parseInt(args.get(1));
-			}
-			catch (NumberFormatException e) {
+			} catch (NumberFormatException e)
+			{
 				plugin.messageBuilder.compose(sender, MessageId.COMMAND_FAIL_QUANTITY_INVALID).send();
 				plugin.soundConfig.playSound(sender, SoundId.COMMAND_FAIL);
 				return true;
@@ -134,7 +147,8 @@ final class GiveSubcommand extends AbstractSubcommand {
 		// validate quantity (min = 1, max = configured maximum, or runtime Integer.MAX_VALUE)
 		quantity = Math.max(1, quantity);
 		int maxQuantity = plugin.getConfig().getInt("max-give-amount");
-		if (maxQuantity < 0) {
+		if (maxQuantity < 0)
+		{
 			maxQuantity = Integer.MAX_VALUE;
 		}
 		quantity = Math.min(maxQuantity, quantity);
@@ -144,12 +158,14 @@ final class GiveSubcommand extends AbstractSubcommand {
 
 		// count items that didn't fit in inventory
 		int noFitCount = 0;
-		for (int index : noFit.keySet()) {
+		for (int index : noFit.keySet())
+		{
 			noFitCount += noFit.get(index).getAmount();
 		}
 
 		// if remaining items equals quantity given, send player-inventory-full message and return
-		if (noFitCount == quantity) {
+		if (noFitCount == quantity)
+		{
 			plugin.messageBuilder.compose(sender, MessageId.COMMAND_FAIL_GIVE_INVENTORY_FULL).send();
 			plugin.soundConfig.playSound(sender, SoundId.COMMAND_FAIL);
 			return true;
@@ -159,8 +175,8 @@ final class GiveSubcommand extends AbstractSubcommand {
 		quantity = quantity - noFitCount;
 
 		// don't display messages if giving item to self
-		if (!sender.getName().equals(targetPlayer.getName())) {
-
+		if (!sender.getName().equals(targetPlayer.getName()))
+		{
 			// send message and play sound to giver
 			plugin.messageBuilder.compose(sender, MessageId.COMMAND_SUCCESS_GIVE)
 					.setMacro(Macro.ITEM_QUANTITY, quantity)
@@ -168,7 +184,8 @@ final class GiveSubcommand extends AbstractSubcommand {
 					.send();
 
 			// if giver is in game, play sound
-			if (sender instanceof Player) {
+			if (sender instanceof Player)
+			{
 				plugin.soundConfig.playSound(sender, SoundId.COMMAND_SUCCESS_GIVE_SENDER);
 			}
 
@@ -178,7 +195,8 @@ final class GiveSubcommand extends AbstractSubcommand {
 					.setMacro(Macro.TARGET_PLAYER, sender)
 					.send();
 		}
-		else {
+		else
+		{
 			// send message when giving to self
 			plugin.messageBuilder.compose(sender, MessageId.COMMAND_SUCCESS_GIVE_SELF)
 					.setMacro(Macro.ITEM_QUANTITY, quantity)
